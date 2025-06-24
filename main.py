b54e816cd247f6e99391500fefde78e3e960f764
Cross, Winter Isaac <wi928rsnowflake@gmail.com>
2025-06-24 12:26:33 -0400

    Added matchup generation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from colorama import Fore, Back, Style
+from functools import cmp_to_key
+import copy
 
 players = [] # player = [matches, lives, name]
 ongoingMatches = [] # match = [playerIndex1, playerIndex2]
@@ -8,18 +10,34 @@ def main():
     # Show the main menu
     print(Style.RESET_ALL)
     print("Welcome to the Swiss Tournament Matchup Generator")
+    auto_populate_players()
     menu()
+
+def auto_populate_players():
+    # Testing function to speed things up
+    players.append([2, 0, 'A'])
+    players.append([2, 1, 'B'])
+    players.append([2, 2, 'C'])
+    players.append([1, 2, 'D'])
+    players.append([1, 1, 'E'])
+    players.append([0, 2, 'F'])
+    players.append([0, 2, 'G'])
+    players.append([0, 2, 'H'])
+    completeMatches.append([0,1])
+    completeMatches.append([0,2])
+    completeMatches.append([1,2])
+    completeMatches.append([3,4])
 
 def menu():
     keepgoing = True
     while keepgoing:
-        print("")
+        print(Fore.MAGENTA)
         print("What would you like to do?")
 
         print("1) Add Players")
         print("2) Display Players\n")
 
-        print("3) Generate Best Next Matchups\n")
+        print("3) Generate Matchups\n")
 
         print("4) Set Ongoing Match")
         print("5) Display Ongoing Matches\n")
@@ -28,7 +46,7 @@ def menu():
         print("7) Display Match Results\n")
 
         print("0) Exit Program")
-        print("")
+        print(Style.RESET_ALL)
 
         choice = input()
         if choice.isdigit():
@@ -40,11 +58,11 @@ def menu():
                 case 1:
                     add_players_menu()
                 case 2:
-                    display_players()
-                # case 3:
-                #     generate_matchups_menu()
+                    display_players(players)
+                case 3:
+                    generate_matchups_menu()
                 case 4:
-                    display_players()
+                    display_players(players) #!!! Should not show players that are dead or in ongoing matches
                     set_ongoing_match()
                 case 5:
                     display_ongoing_matches()
@@ -62,6 +80,7 @@ def menu():
             print(Fore.RED + "That was not a valid option")
             print(Style.RESET_ALL)
 
+# 1) Add Players
 def add_players_menu():
     # Ask how many players the user wants to add
     print("How many players would you like to add?")
@@ -75,62 +94,214 @@ def add_players_menu():
     for i in range(numPlayers):
         add_player_menu()
 
-    print(Fore.GREEN + str(numPlayers) + " player added successfully!")
+    print(Fore.GREEN + str(numPlayers) + " players added successfully!")
     print(Style.RESET_ALL)
 def add_player_menu():
     print("What is this player's name?")
     name = input()
     players.append([0, 2, name])
 
-def display_players():
+# 2) Display Players
+def display_players(playerList):
     print(Fore.YELLOW + f"{"Index":^10} {"Name":^20} {"Matches":^10} {"Lives":^10}")
-    for i in range(len(players)):
-        matches = players[i][0]
-        name = players[i][2]
-        lives = players[i][1]
+    for i in range(len(playerList)):
+        matches = playerList[i][0]
+        name = playerList[i][2]
+        lives = playerList[i][1]
         if lives == 0:
             print(Fore.RED + f"{i:^10} {name:^20} {matches:^10} {lives:^10}" + Fore.YELLOW)
         else:
             print(f"{i:^10} {name:^20} {matches:^10} {lives:^10}")
     print(Style.RESET_ALL)
 
-# def generate_matchups_menu():
-#     #!!! Think deeply about how this menu ought to work
-#     keepgoing = True
-#     while keepgoing:
-#         print("")
-#         print("What matchup would you like to generate?")
-#         print("1) Best Matchup")
-#         print("2) Best Matchups With Player")
+# 3) Generate Matchups
+def generate_matchups_menu():
+    keepgoing = True
+    while keepgoing:
+        print(Fore.MAGENTA)
+        print("What matchup would you like to generate?")
+        print("1) Display Primed Players")
+        print("2) Best Matchup\n")
 
-#         print("0) Exit Matchup Generation")
-#         print("")
-
-
-#         choice = input()
-#         if choice.isdigit():
-#             choice = int(choice)
-#             match choice:
-#                 case 0:
-#                     keepgoing = False
-#                 case 1:
-#                     print("How many first players should by tried?")
-#                     numFirstPlayers = input()
-#                     print("How many second players should be tried?")
-#                     numSecondPlayers = input()
-
-#                     if numFirstPlayers.isdigit() and numSecondPlayers.isdigit():
-#                         numFirstPlayers = int(numFirstPlayers)
-#                         numSecondPlayers = int(numSecondPlayers)
-#                         display_best_matchups(numFirstPlayers, numSecondPlayers)
-#                     else:
-#                         print("Input numbers stupid")
-#                 case _:
-#                     print("That was not a valid option")
-#         else:
-#             print("That was not a valid option")
+        print("0) Exit Matchup Generation")
+        print(Style.RESET_ALL)
 
 
+        choice = input()
+        if choice.isdigit():
+            choice = int(choice)
+            match choice:
+                case 0:
+                    keepgoing = False
+                case 1:
+                    display_primed_players()
+                case 2:
+                    best_matchup_menu()
+                case _:
+                    print("That was not a valid option")
+        else:
+            print("That was not a valid option")
+
+def display_primed_players():
+    # Sorts the list of players in order of primedness and prints it
+    primedPlayerIndeces = list(range(len(players)))
+    primedPlayerIndeces = sorted(primedPlayerIndeces, key=cmp_to_key(compare))
+
+    primedPlayers = []
+    for playerIndex in primedPlayerIndeces:
+        primedPlayers.append(players[playerIndex])
+
+    print(Fore.YELLOW + f"{"Name":^20} {"Matches":^10} {"Lives":^10}")
+    for i in range(len(primedPlayers)):
+        matches = primedPlayers[i][0]
+        name = primedPlayers[i][2]
+        lives = primedPlayers[i][1]
+        if lives == 0:
+            print(Fore.RED + f"{name:^20} {matches:^10} {lives:^10}" + Fore.YELLOW)
+        else:
+            print(f"{name:^20} {matches:^10} {lives:^10}")
+    print(Style.RESET_ALL)
+
+def compare(p1Index, p2Index):
+    # Returns -1 if p1 more primed, 1 if p2 more primed, 0 if equal
+    p1 = players[p1Index]
+    p2 = players[p2Index]
+    matches1 = p1[0]
+    matches2 = p2[0]
+    lives1 = p1[1]
+    lives2 = p2[1]
+
+    if lives1 != 0 and lives2 == 0:
+        return -1
+    elif lives1 == 0 and lives2 != 0:
+        return 1
+
+    if matches1 < matches2:
+        return -1
+    elif matches1 > matches2:
+        return 1
+
+    if lives1 < lives2:
+        return -1
+    elif lives1 > lives2:
+        return 1
+
+    return 0
+
+def best_matchup_menu():
+    # Finds the best legal matchup, if possible. 
+    # Lets the user remove one of the players in the found match to generate a new one
+
+    excludeIndeces = []
+    keepgoing = True
+    while keepgoing:
+        index1 = find_primed_player_index(excludeIndeces)
+        index2 = find_primed_player_index(excludeIndeces, index1)
+        if index1==-1 or index2==-1:
+            print(Fore.RED + "No legal matchups exist!")
+            print(Style.RESET_ALL)
+            return
+        print(f"{str(index1) + " " + players[index1][2] + " would face " + str(index2) + " " + players[index2][2]:^30}")
+
+        print(Fore.MAGENTA)
+        print("1) Remove Player 1")
+        print("2) Remove Player 2\n")
+
+        print("0) Exit matchup Menu")
+        print(Style.RESET_ALL)
+
+        choice = input()
+        if choice.isdigit():
+            choice = int(choice)
+            match choice:
+                case 0:
+                    keepgoing = False
+                case 1:
+                    excludeIndeces.append(index1)
+                case 2:
+                    excludeIndeces.append(index2)
+                case _:
+                    print("That was not a valid option")
+        else:
+            print("That was not a valid option")
+
+def find_primed_player_index(excludeIndeces = [], illegalPastOpponentIndex = -1): #!!!
+    # Finds the player with the fewest matches and fewest lives that is alive, not in exlcudeIndeces, is not illegalPastOpponent, 
+    # not in an ongoing match. Prioritize players that have not faced illegalPastOpponent, but it's okay if there are no other options.
+    # If impossible, returns -1
+
+    primedPlayerIndex = -1
+    # Narrow down the list of possible players
+    eligiblePlayerIndeces = list(range(len(players)))
+    eligiblePlayerIndeces = get_living_players(eligiblePlayerIndeces)
+    eligiblePlayerIndeces = get_unexcluded_players(eligiblePlayerIndeces, excludeIndeces)
+    if illegalPastOpponentIndex != -1:
+        eligiblePlayerIndeces = get_unexcluded_players(eligiblePlayerIndeces, [illegalPastOpponentIndex])
+    eligiblePlayerIndeces = get_unoccupied_players(eligiblePlayerIndeces, ongoingMatches)
+    if illegalPastOpponentIndex != -1:
+        eligiblePlayerIndeces = get_fewest_times_played_opponent(eligiblePlayerIndeces, illegalPastOpponentIndex)
+
+    # Find the most primed player of the remaining list
+    if len(eligiblePlayerIndeces) > 0:
+        eligiblePlayerIndeces = sorted(eligiblePlayerIndeces, key=cmp_to_key(compare)) 
+        primedPlayerIndex = eligiblePlayerIndeces[0] #!!! Should be random
+
+    return primedPlayerIndex
+
+def get_living_players(playerIndecesList):
+    newPlayerIndecesList = []
+    for playerIndex in playerIndecesList:
+        player = players[playerIndex]
+        lives = player[1]
+        if lives < 1:
+            continue
+        newPlayerIndecesList.append(playerIndex)
+    return newPlayerIndecesList
+def get_unoccupied_players(playerIndecesList, ongoingMatchList):
+    newPlayerIndecesList = []
+    for playerIndex in playerIndecesList:
+        player = players[playerIndex]
+        playerInMatch = False
+        for match in ongoingMatchList:
+            if player in match:
+                playerInMatch = True
+                break
+        if playerInMatch:
+            continue
+        newPlayerIndecesList.append(playerIndex)
+    return newPlayerIndecesList
+def get_unexcluded_players(playerIndecesList, excludeIndeces):
+    newPlayerIndecesList = []
+    for playerIndex in playerIndecesList:
+        if playerIndex in excludeIndeces:
+            continue
+        newPlayerIndecesList.append(playerIndex)
+    return newPlayerIndecesList
+def get_fewest_times_played_opponent(playerIndecesList, opponentIndex):
+    # Returns the subset of playerIndecesList with the fewest number of times playing with opponentIndex in the past
+    # If none have played, all are included in the output
+    newPlayerIndecesList = []
+    opponentMatches = []
+    for match in completeMatches:
+        if opponentIndex in match:
+            opponentMatches.append(match)
+
+    fewestMatches = 99
+    for playerIndex in playerIndecesList:
+        numMatches = 0
+        for match in opponentMatches:
+            if playerIndex in match:
+                numMatches+=1
+        if numMatches < fewestMatches:
+            fewestMatches = numMatches
+            newPlayerIndecesList = []
+            newPlayerIndecesList.append(playerIndex)
+        elif numMatches == fewestMatches:
+            newPlayerIndecesList.append(playerIndex)
+
+    return newPlayerIndecesList
+
+# 4) Set Ongoing Match
 def set_ongoing_match():
     # Have the player type the indeces of two players and save it as an ongoing match
     # Check if the pairing is legal (i.e. neither player is already in a match)
@@ -168,6 +339,7 @@ def set_ongoing_match():
     print(Fore.GREEN + "The match between " + str(player1Index) + " " + players[player1Index][2] + " and " + str(player2Index) + " " + players[player2Index][2] + " has been added!")
     print(Style.RESET_ALL)
 
+# 5) Display Ongoing Matches
 def display_ongoing_matches():
     print(Fore.YELLOW + f"{"Index":^5}" + f"{"Matchup":^30}")
     for matchIndex in range(len(ongoingMatches)):
@@ -178,6 +350,7 @@ def display_ongoing_matches():
         print(f"{str(matchIndex):^5}" + f"{str(p1Index) + " " + p1Name + " is facing " + str(p2Index) + " " + p2Name:^30}")
     print(Style.RESET_ALL)
 
+# 6) Set Match Result
 def set_match_result():
     # Ask the player which to resolve
     print("Which match would you like to resolve?")
@@ -225,6 +398,7 @@ def set_match_result():
     completeMatches.append([winIndex, loseIndex])
     ongoingMatches.pop(matchChoice)
 
+# 7) Display Match Results
 def display_match_results():
     print(Fore.YELLOW + "Results")
     for match in completeMatches:
@@ -244,7 +418,8 @@ def display_match_results():
     # print(Style.RESET_ALL)
 
 
-def secret_menu(): #!!! To be used to edit matches as well
+# 91) Secret Menu
+def secret_menu():
     print(Fore.BLUE + "Welcome to the Secret Area!")
 
     keepgoing = True
@@ -270,7 +445,6 @@ def secret_menu(): #!!! To be used to edit matches as well
                 case 0:
                     keepgoing = False
                     print(Style.RESET_ALL)
-
                 case 1:
                     set_input_player_values()
                 case 2:
@@ -290,7 +464,7 @@ def secret_menu(): #!!! To be used to edit matches as well
             print(Fore.RED + "That was not a valid option" + Fore.BLUE)
 
 def set_input_player_values():
-    display_players()
+    display_players(players)
     print(Fore.BLUE)
 
     print("Which player would you like to edit?")
@@ -323,7 +497,7 @@ def set_input_player_values():
     players[playerIndex][1] = int(lives)
 
 def delete_player(): #!!! Must alter indeces in matches when deleting player
-    display_players()
+    display_players(players)
     print(Fore.BLUE)
 
     print("Which player would you like to delete?")
@@ -354,7 +528,7 @@ def set_input_ongoing_match_values():
         print(Fore.RED + "There are not that many ongoing matches" + Fore.BLUE)
         return
 
-    display_players()
+    display_players(players)
     print(Fore.BLUE)
 
     print("What is the first player's index?")
@@ -402,7 +576,7 @@ def set_input_complete_match_values():
         print(Fore.RED + "There are not that many complete matches" + Fore.BLUE)
         return
 
-    display_players()
+    display_players(players)
     print(Fore.BLUE)
 
     print("What is the winning player's index?")
@@ -435,39 +609,6 @@ def delete_complete_match():
     
     completeMatches.pop(matchIndex)
 
-# def display_best_matchups(numFirstPlayers: int, numSecondPlayers: int):
-#     matchups = []
-
-# def get_best_matchup(player1Index = -1, excludeIndeces = []):
-#     # Finds the best matchup satisfying all the restrictions set in the parameters
-    
-#     if player1Index!=-1:
-#         index1 = player1Index
-#     else:
-#         index1 = find_primed_player_index(excludeIndeces)
-
-#     newExcludeIndeces = list( set(excludeIndeces).union(set([index1])) )
-#     index2 = find_primed_player_index(newExcludeIndeces)
-
-#     return [index1, index2]
-
-# def find_primed_player_index(excludeIndeces = []):
-#     # Finds the player with the fewest matches and fewest lives. We call this the primed player
-#     primedPlayer = -1
-#     for i in range(len(players)):
-#         if i in excludeIndeces:
-#             continue
-#         if primedPlayer == -1:
-#             primedPlayer = i
-#             continue
-#         if (players[i][0] < players[primedPlayer][0]) or ((players[i][0] == players[primedPlayer][0]) and (players[i][1] < players[primedPlayer][1])):
-#             primedPlayer = i
-#     return primedPlayer
-# Helper functions
-# def int_to_letter(value):
-#     # Maps integers to capital letters. 0 to A, 1 to B, etc.
-#     #!!! Values greater than 25 will no longer be letters
-#     return chr(value + ord("A"))
 
 if __name__=='__main__':
     main()
